348bf2d4e169360c9437843561f6d347ad4389dd
ducnguyenn2901 <nmducwin@gmail.com>
2025-09-12 10:36:16 +0700

    update2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,3 @@
-// import { Link } from 'react-router-dom'
-
-// export default function Navbar(){
-//   return (
-//     <div className="navbar">
-//       <div className="navbar-inner container">
-
-//         <Link className="button" to="/"><i class="fa-solid fa-house"></i></Link>
-//         <Link to="/" className="brand">Anonime</Link>
-//         <div className="nav-links">
-//           <Link to="/">Home</Link>
-//           <Link to="/list">List anime</Link>
-//           <Link to="/login">Account</Link>
-//         </div>
-//         <div className="search">
-//           <input placeholder="Search anime or movie" />
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
